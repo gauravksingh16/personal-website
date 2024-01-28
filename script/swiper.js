ce41bed04fcdcd7944c0555cc7c33613ea3c224a
gauravksingh16 <gaurav.12007335@lpu.in>
2024-01-29 03:27:55 +0530

    work experience

--- script/swiper.js
+++ script/swiper.js
@@ -1,4 +1,24 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 50,
-  });
+  slidesPerView: 2,
+  spaceBetween: 50,
+});
+
+var swipeContainer = document.querySelector(".swiper");
+var mouseBtn = document.getElementById("mouse-navigator");
+
+swipeContainer.addEventListener("mouseenter", function (event) {
+  mouseBtn.style.opacity = 1;
+  mouseBtn.style.scale = 1;
+  mouseBtn.style.top = event.clientY + "px";
+  mouseBtn.style.left = event.clientX + "px";
+});
+
+swipeContainer.addEventListener("mouseleave", function () {
+  mouseBtn.style.opacity = 0;
+  mouseBtn.style.scale = 0;
+});
+
+swipeContainer.addEventListener("mousemove", function (event) {
+  mouseBtn.style.top = event.clientY + "px";
+  mouseBtn.style.left = event.clientX + "px";
+});
