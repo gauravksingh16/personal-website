7f5285876fb3bde532147015ad89a859f3d76ed9
gauravksingh16 <gaurav.12007335@lpu.in>
2024-01-31 13:54:17 +0530

    responsive update 2

--- script/video.js
+++ script/video.js
@@ -4,35 +4,41 @@ var video = document.getElementById("video-player");
 const musicBtn = document.getElementById("musicButton");
 const audioplay = document.getElementById("background-music");
 
-videocon.addEventListener("mouseenter", function (event) {
-  playbtn.style.opacity = 1;
-  playbtn.style.scale = 1;
-  playbtn.style.top = event.clientY + "px";
-  playbtn.style.left = event.clientX + "px";
-});
+function setupVideoControls() {
+  videocon.addEventListener("mouseenter", function (event) {
+    playbtn.style.opacity = 1;
+    playbtn.style.scale = 1;
+    playbtn.style.top = event.clientY + "px";
+    playbtn.style.left = event.clientX + "px";
+  });
 
-videocon.addEventListener("mouseleave", function () {
-  playbtn.style.opacity = 0;
-  playbtn.style.scale = 0;
-});
+  videocon.addEventListener("mouseleave", function () {
+    playbtn.style.opacity = 0;
+    playbtn.style.scale = 0;
+  });
 
-videocon.addEventListener("mousemove", function (event) {
-  playbtn.style.top = event.clientY + "px";
-  playbtn.style.left = event.clientX + "px";
-});
+  videocon.addEventListener("mousemove", function (event) {
+    playbtn.style.top = event.clientY + "px";
+    playbtn.style.left = event.clientX + "px";
+  });
 
-videocon.addEventListener("click", function (event) {
-  if (video.paused) {
-    musicBtn.classList.toggle("active");
-    audioplay.pause();
-    video.play();
-    video.muted = false
-    playbtn.innerHTML = "PAUSE"
-  } else {
-    musicBtn.classList.toggle("active");
-    video.pause();
-    audioplay.play();
-    audioplay.volume = 0.25
-    playbtn.innerHTML = "PLAY"
-  }
-});
+  videocon.addEventListener("click", function () {
+    if (video.paused) {
+      musicBtn.classList.toggle("active");
+      audioplay.pause();
+      video.play();
+      video.muted = false;
+      playbtn.innerHTML = "PAUSE";
+    } else {
+      musicBtn.classList.toggle("active");
+      video.pause();
+      audioplay.play();
+      audioplay.volume = 0.25;
+      playbtn.innerHTML = "PLAY";
+    }
+  });
+}
+
+if(screen.width > 890) {
+  setupVideoControls();
+}
