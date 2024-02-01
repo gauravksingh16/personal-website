1d077139d336ec94e6a2ed1e81a582e01989a6d0
gauravksingh16 <gaurav.12007335@lpu.in>
2024-02-01 21:50:12 +0530

    newsletter signup

--- script/script.js
+++ script/script.js
@@ -11,3 +11,19 @@ function clickEffect(e){
 document.addEventListener('click',clickEffect);
 
 window.addEventListener('load', function() {  window.scroll(0, 0) })
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('newsletterform');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Thank You for Subscribing!");
+      })
+    });
+  });
